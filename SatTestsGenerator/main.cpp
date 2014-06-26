8bf57a1da4859b6f4ece9d4f0407d2aef9cbccb7
Nauchnik <zaikin.icc@gmail.com>
2014-06-26 18:22:51 +0900

    + class, + correct head of cnf

--- SatTestsGenerator/main.cpp
+++ SatTestsGenerator/main.cpp
@@ -11,6 +11,8 @@
 #include <boost/random/mersenne_twister.hpp>
 #include <boost/random/uniform_int_distribution.hpp>
 
+#include "mpi_base.h"
+
 using namespace std;
 
 boost::random::mt19937 gen(static_cast<unsigned int>(std::time(0)));
@@ -20,61 +22,107 @@ int bool_rand() {
 	return dist(gen);
 }
 
-int main( int argc, char **argv )
+class makeSample
 {
-	// debug
-	/*argc = 4;
-	argv[1] = "./bivium_test_0.cnf";
-	argv[2] = "decomp_set.txt";
-	argv[3] = "10";*/
-
-	if ( argc < 4 ) {
-		cout << "Usage: [cnf_file] [decomp_set_file] tests_count";
-		return 1;
-	}
-
-	int tests_count;
+public:
+	makeSample();
+	bool readInput( int argc, char **argv );
+	bool init();
+	bool ifSatSample();
+	void makeUnsatSample();
+private:
+	unsigned tests_count;
 	string cnf_file_name;
-	cnf_file_name = argv[1];
-	
-	ifstream cnf_file( cnf_file_name.c_str() );
-	if ( !cnf_file.is_open() ) {
-		cout << "Error. !cnf_file.is_open()" << endl;
-		return 1;
-	}
+	string decomp_set_file_name;
+	string cnf_name_common_part;
+	vector<ofstream*> test_cnf_files;
+	vector<int> decomp_set;
+	bool isSatSample;
+	ifstream cnf_file;
+	ifstream decomp_set_file;
+	stringstream comment_cnf_sstream;
+	stringstream head_cnf_sstream;
+	stringstream main_cnf_sstream;
+	vector<vector<bool>> state_vec_vec, stream_vec_vec;
+};
 
+makeSample :: makeSample() :
+	isSatSample (false)
+{}
+
+bool makeSample :: ifSatSample() { return isSatSample; }
+
+bool makeSample :: readInput( int argc, char **argv )
+{
+	if ( argc < 4 ) {
+		cout << "Usage: cnf_file decomp_set_file tests_count [-sat]";
+		return false;
+	}
+	
+	cnf_file_name = argv[1];
+	cout << "cnf_file_name " << cnf_file_name << endl;
 	while ( (cnf_file_name[0] == '.') || ( cnf_file_name[0] == '/') )
 	    cnf_file_name = cnf_file_name.substr(1, cnf_file_name.length() - 1);
-
 	cout << "new cnf_file_name " << cnf_file_name << endl;
-	ifstream decomp_set_file( argv[2] );
+	
+	decomp_set_file_name = argv[2];
+	cout << "decomp_set_file_name " << decomp_set_file_name << endl;
+
+	tests_count = atoi( argv[3] );
+	cout << "tests_count " << tests_count << endl;
+	
+	string str;
+	if ( argc > 4 ) {
+		str = argv[4];
+		if ( str.find( "sat" ) != string::npos )
+			isSatSample = true;
+	}
+	cout << "isSatSample " << isSatSample << endl;
+
+	return true;
+}
+
+bool makeSample :: init()
+{
+	cnf_file.open( cnf_file_name.c_str() );
+	if ( !cnf_file.is_open() ) {
+		cout << "Error. !cnf_file.is_open()" << endl;
+		return false;
+	}
+	
+	decomp_set_file.open( decomp_set_file_name.c_str() );
 	if ( !decomp_set_file.is_open() ) {
 		cout << "Error. !cnf_file.is_open()" << endl;
-		return 1;	
-	}
-	tests_count = atoi( argv[3] );
-	if ( tests_count <= 0 ) {
-		cout << "Error. tests_count <= 0" << endl;
-		return 1;
+		return false;	
 	}
 
-	stringstream head_cnf_sstream, main_cnf_sstream;
+	if ( tests_count <= 0 ) {
+		cout << "Error. tests_count <= 0" << endl;
+		return false;
+	}
+
+	test_cnf_files.resize( tests_count );
+
+	unsigned found = cnf_file_name.find( "." );
+	cnf_name_common_part = (found != string::npos) ? cnf_file_name.substr( 0, found ) : cnf_file_name;
+	cout << "cnf_name_common_part " << cnf_name_common_part << endl;
+
+	unsigned comment_str_count = 0, main_str_count = 0;
 	string str;
-	unsigned head_str_count = 0, main_str_count = 0;
 	while ( getline( cnf_file, str ) ) {
-		if ( ( str[0] == 'c' ) || ( str[0] == 'p' ) ) {
-			head_cnf_sstream << str << endl;
-			head_str_count++;
+		if ( str[0] == 'c' ) {
+			comment_cnf_sstream << str << endl;
+			comment_str_count++;
 		}
-		else {
-			main_cnf_sstream << str << endl;
+		else if ( str[0] != 'p' ) {
+			if ( main_str_count )
+				main_cnf_sstream << endl;
+			main_cnf_sstream << str;
 			main_str_count++;
 		}
 	}
-	cout << "head_str_count " << head_str_count << endl;
 	cout << "main_str_count " << main_str_count << endl;
 
-	vector<int> decomp_set;
 	int val;
 	cout << "reading decomp_set ";
 	while ( !decomp_set_file.eof() ) {
@@ -85,22 +133,40 @@ int main( int argc, char **argv )
 	cout << endl;
 	cout << "decomp_set.size() " << decomp_set.size() << endl; 
 
-	decomp_set_file.close();
+	MPI_Base mpi_b;
+	mpi_b.isMakeSatSampleAnyWay = true;
+	mpi_b.input_cnf_name = new char[cnf_file_name.size() + 1];
+	strcpy( mpi_b.input_cnf_name, cnf_file_name.c_str() );
+	mpi_b.ReadIntCNF();
+	delete[] mpi_b.input_cnf_name;
+	cout << "mpi_b.var_count " << mpi_b.var_count << endl;
+	cout << "mpi_b.clause_count "  << mpi_b.clause_count  << endl;
 	
-	vector<ofstream*> test_cnf_files;
-	test_cnf_files.resize( tests_count );
+	unsigned new_clause_count;
+
+	if ( isSatSample ) {
+		mpi_b.cnf_in_set_count = tests_count;
+		mpi_b.MakeSatSample( state_vec_vec, stream_vec_vec );
+		new_clause_count = mpi_b.clause_count + state_vec_vec[0].size() + stream_vec_vec[0].size();
+	}
+	else
+		new_clause_count = mpi_b.clause_count + decomp_set.size();
+	
+	head_cnf_sstream << "p cnf " << mpi_b.var_count << " " << new_clause_count << endl;
+	
+	decomp_set_file.close();
+	cnf_file.close();
+
+	return true;
+}
+
+void makeSample :: makeUnsatSample()
+{
 	stringstream current_name_sstream;
-
-	unsigned found = cnf_file_name.find( "." );
-	string cnf_name_common_part;
-	if ( found != string::npos )
-		cnf_name_common_part = cnf_file_name.substr( 0, found );
-	else 
-		cnf_name_common_part = cnf_file_name;
-
 	for ( unsigned i = 0; i < test_cnf_files.size(); i++ ) {
 		current_name_sstream << cnf_name_common_part << "_" << i << ".cnf";
 		test_cnf_files[i] = new ofstream( current_name_sstream.str().c_str() );
+		(*test_cnf_files[i]) << comment_cnf_sstream.str(); // write comments of cnf file
 		(*test_cnf_files[i]) << head_cnf_sstream.str(); // write head of cnf file
 		// write oneliteral clauses
 		for ( unsigned j = 0; j < decomp_set.size(); j++ ) {
@@ -114,9 +180,22 @@ int main( int argc, char **argv )
 		current_name_sstream.clear(); current_name_sstream.str("");
 	}
 	cout << tests_count << " tests were created" << endl;
+}
 
-	cnf_file.close();
-  
+int main( int argc, char **argv )
+{
+	// debug
+	argc = 4;
+	argv[1] = "./bivium_test_0.cnf";
+	argv[2] = "decomp_set.txt";
+	argv[3] = "2";
+
+	makeSample make_s;
+	make_s.readInput( argc, argv );
+	make_s.init();
+	if ( !make_s.ifSatSample() )
+		make_s.makeUnsatSample();
+	
 	//system("pause");
 	return 0;
 }
