491a3d420b9673b41a3b00f8f2282d725a2a3fea
Oleg Zaikin <zaikin.icc@gmail.com>
2016-02-06 23:47:45 +0800

    * fixing CNF

--- FixCNF/fix_cnf.cpp
+++ FixCNF/fix_cnf.cpp
@@ -5,8 +5,8 @@
 #include <vector>
 #include <algorithm>
 
-const unsigned clauses_block_size = 100000;
-const unsigned max_blocks_number = 1000;
+const unsigned CLAUSES_BLOCK_SIZE = 100000;
+const unsigned MAX_BLOCKS_NUMBER = 1000;
 
 int main( int argc, char **argv )
 {
@@ -29,7 +29,7 @@ int main( int argc, char **argv )
 	std::string str;
 	std::stringstream comment_cnf_sstream, sstream;
 	std::vector<std::stringstream*> main_cnf_sstream_vec;
-	main_cnf_sstream_vec.resize(max_blocks_number);
+	main_cnf_sstream_vec.resize(CLAUSES_BLOCK_SIZE);
 	for (auto &x : main_cnf_sstream_vec)
 		x = new std::stringstream();
 	comment_cnf_sstream << "";
@@ -37,8 +37,12 @@ int main( int argc, char **argv )
 	unsigned cur_clauses_number = 0, block_index = 0;
 	unsigned comment_str_count = 0, var_count = 0, clause_count = 0;
 	int lit;
+	int pos;
 	while ( getline(cnf_file, str) ) {
 		str.erase( std::remove(str.begin(), str.end(), '\r'), str.end() );
+		pos = str.find(" 0 ");
+		if ( pos != std::string::npos )
+			str.resize(pos+2);
 		if ( str[0] == 'c' ) {
 			comment_cnf_sstream << str << std::endl;
 			comment_str_count++;
@@ -49,7 +53,7 @@ int main( int argc, char **argv )
 			*(main_cnf_sstream_vec[block_index]) << str;
 			cur_clauses_number++;
 			// add full current block to vector
-			if (cur_clauses_number == clauses_block_size) {
+			if (cur_clauses_number == CLAUSES_BLOCK_SIZE) {
 				cur_clauses_number = 0;
 				block_index++;
 			}
@@ -64,17 +68,18 @@ int main( int argc, char **argv )
 	}
 	*(main_cnf_sstream_vec[block_index]) << " "; // add space on the last string for treengeling
 	std::cout << "CNF processed" << std::endl;
-	std::cout << "clauses_block_size " << clauses_block_size << std::endl;
+	std::cout << "clauses_block_size " << CLAUSES_BLOCK_SIZE << std::endl;
 	std::cout << "block_index " << block_index << std::endl;
 	std::cout << "clauses_number " << cur_clauses_number << std::endl;
-	clause_count = clauses_block_size*block_index + cur_clauses_number;
+	clause_count = CLAUSES_BLOCK_SIZE*block_index + cur_clauses_number;
 	cnf_file.close();
 	cnf_file.clear();
 	
 	// delete old data from old file and write updated data to it
 	cnf_file.open( cnf_name.c_str(), std::ios_base::out );
+	//cnf_file.open( "out.txt", std::ios_base::out );
 	cnf_file << comment_cnf_sstream.str();
-	cnf_file << "p cnf " << var_count << " " << clause_count;
+	cnf_file << "p cnf " << var_count << " " << clause_count << std::endl;
 	for (auto x : main_cnf_sstream_vec)
 		cnf_file << (*x).rdbuf();
 	cnf_file.close();
