bd1b0912581a0b58a6d00a503b7935cd7fe68eb2
Oleg Zaikin <zaikin.icc@gmail.com>
2016-08-02 13:03:53 +0800

    + comparison by total sum of times

--- CompareDLScounters/Source.cpp
+++ CompareDLScounters/Source.cpp
@@ -32,28 +32,32 @@ int main( int argc, char* argv[])
 		return 1;
 	}
 	
-	std::string eduard_file_name = argv[1];
-	std::string stepa_file_name = argv[2];
-	std::vector<wu> wu_vec_eduard = readDataFromFile(eduard_file_name);
-	std::vector<wu> wu_vec_stepa = readDataFromFile(stepa_file_name);
-
+	std::string file1_name = argv[1];
+	std::string file2_name = argv[2];
+	std::vector<wu> wu_vec_1 = readDataFromFile(file1_name);
+	std::vector<wu> wu_vec_2 = readDataFromFile(file2_name);
+	
 	bool is_calculated_results_correct = true;
 	unsigned calculated_results_both_files = 0;
 	double comparison_koef_sum = 0;
 	double cur_comparison_koef;
-	for (unsigned i = 0; i < wu_vec_eduard.size(); i++) {
-		if ((wu_vec_eduard[i].dls_number >= 0) && (wu_vec_stepa[i].dls_number >= 0)) {
+	double calculated_sum1_time = 0, calculated_sum2_time;
+	for (unsigned i = 0; i < wu_vec_1.size(); i++) {
+		if ((wu_vec_1[i].dls_number >= 0) && (wu_vec_2[i].dls_number >= 0)) {
+			calculated_sum1_time += wu_vec_1[i].processing_time;
+			calculated_sum2_time += wu_vec_2[i].processing_time;
 			calculated_results_both_files++;
-			cur_comparison_koef = (double)wu_vec_eduard[i].processing_time / (double)wu_vec_stepa[i].processing_time;
-			if ( (cur_comparison_koef < 1) && (wu_vec_eduard[i].dls_number > 0) ) {
+			cur_comparison_koef = (double)wu_vec_1[i].processing_time / (double)wu_vec_2[i].processing_time;
+			if ( (cur_comparison_koef < 1) && (wu_vec_1[i].dls_number > 0) ) {
 				std::cout << "cur_comparison_koef < 1 " << cur_comparison_koef << std::endl;
-				std::cout << "eduard : " << wu_vec_eduard[i].first_cells_known_values << " " << wu_vec_eduard[i].dls_number << " " << wu_vec_eduard[i].processing_time << std::endl;
-				std::cout << "stepa : " << wu_vec_stepa[i].first_cells_known_values << " " << wu_vec_stepa[i].dls_number << " " << wu_vec_stepa[i].processing_time << std::endl;
+				std::cout << "file1 : " << wu_vec_1[i].first_cells_known_values << " " << wu_vec_1[i].dls_number << " " << wu_vec_1[i].processing_time << std::endl;
+				std::cout << "file2 : " << wu_vec_2[i].first_cells_known_values << " " << wu_vec_2[i].dls_number << " " << wu_vec_2[i].processing_time << std::endl;
 				std::cout << std::endl;
 			}
-			if (wu_vec_eduard[i].dls_number != wu_vec_stepa[i].dls_number)
+			if (wu_vec_1[i].dls_number != wu_vec_2[i].dls_number)
 				is_calculated_results_correct = false;
 			comparison_koef_sum += cur_comparison_koef;
+			std::cout << "cur_comparison_koef " << cur_comparison_koef << std::endl;
 		}
 	}
 	
@@ -62,6 +66,9 @@ int main( int argc, char* argv[])
 	std::cout << "calculated_results_both_files " << calculated_results_both_files << std::endl;
 	std::cout << "is_calculated_results_correct " << is_calculated_results_correct << std::endl;
 	std::cout << "comparison_koef_average "		  << comparison_koef_average	   << std::endl;
+	std::cout << "calculated_sum1_time "	      << calculated_sum1_time          << std::endl;
+	std::cout << "calculated_sum2_time "          << calculated_sum2_time          << std::endl;
+	std::cout << "sum_koef (calculated_sum1_time / calculated_sum2_time) " << calculated_sum1_time / calculated_sum2_time << std::endl;
 	
 	return 0;
 }
