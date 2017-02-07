61c551278442293c5d82cdedbb926470e8551dc8
Oleg Zaikin <zaikin.icc@gmail.com>
2017-02-07 21:37:18 +0800

    *

--- SMAC_instances_list/Source.cpp
+++ SMAC_instances_list/Source.cpp
@@ -7,8 +7,15 @@
 int main()
 {
 	std::stringstream sstream;
-	for (unsigned i = 0; i < 100; i++)
-		sstream << "instances/BAR/250/GR_CNF/BAR250_" << i << "_gr.cnf" << std::endl;
+	/*for (unsigned i = 0; i < 100; i++)
+		sstream << "instances/BAR/250/GR_CNF/BAR250_" << i << "_gr.cnf" << std::endl;*/
+	unsigned val_number = 36;
+	for (unsigned i = 0; i < val_number; i++) {
+		sstream << 500 + i * 100;
+		if (i<val_number-1)
+			sstream << ",";
+	}
+
 	std::ofstream ofile("out.txt");
 	ofile << sstream.rdbuf();
 	ofile.close();
