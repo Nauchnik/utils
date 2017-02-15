c9620f608c9505d00633edc385bd452590156675
Oleg Zaikin <zaikin.icc@gmail.com>
2017-02-15 19:27:10 +0800

    + making instances lists for SMAC

--- SMAC_instances_list/Source.cpp
+++ SMAC_instances_list/Source.cpp
@@ -4,21 +4,65 @@
 #include <fstream>
 #include <vector>
 
+void makeInstancesLists(char *problem_name_char_arr, unsigned vertices_number, unsigned instances_number);
+
 int main()
 {
-	std::stringstream sstream;
-	/*for (unsigned i = 0; i < 100; i++)
-		sstream << "instances/BAR/250/GR_CNF/BAR250_" << i << "_gr.cnf" << std::endl;*/
-	unsigned val_number = 36;
-	for (unsigned i = 0; i < val_number; i++) {
-		sstream << 500 + i * 100;
-		if (i<val_number-1)
-			sstream << ",";
-	}
-
-	std::ofstream ofile("out.txt");
-	ofile << sstream.rdbuf();
-	ofile.close();
+	makeInstancesLists("BAR", 500,  100);
+	makeInstancesLists("BAR", 1000, 100);
+	makeInstancesLists("BAR", 2000, 10);
+	makeInstancesLists("BAR", 4000, 10);
+	makeInstancesLists("GNP", 500,  100);
+	makeInstancesLists("GNP", 1000, 100);
+	makeInstancesLists("GNP", 2000, 10);
+	makeInstancesLists("GNP", 4000, 10);
 	
 	return 0;
 }
+
+void makeInstancesLists(char *problem_name_char_arr, unsigned vertices_number, unsigned instances_number)
+{
+	std::string problem_name = problem_name_char_arr;
+	std::stringstream sstream;
+	std::ofstream ofile;
+	std::string ofile_name;
+	std::string cur_encoding;
+	std::string add_instance_name;
+	
+	for (unsigned encoding_index = 0; encoding_index < 4; encoding_index++) {
+		switch (encoding_index) {
+		case 0: 
+			cur_encoding = "CNF";
+			add_instance_name = "";
+			break;
+		case 1: 
+			cur_encoding = "GR_CNF"; 
+			add_instance_name = "_gr";
+			break;
+		case 2: 
+			cur_encoding = "RED_CNF";
+			add_instance_name = "_re";
+			break;
+		case 3: 
+			cur_encoding = "GR_RED_CNF"; 
+			add_instance_name = "_gr_re";
+			break;
+		default: 
+			cur_encoding = "CNF"; 
+			add_instance_name = "";
+			break;
+		}
+		sstream << problem_name << "_" << vertices_number << "_" << cur_encoding << "_all";
+		ofile_name = sstream.str();
+		sstream.str(""); sstream.clear();
+		for (unsigned i = 0; i < instances_number; i++)
+			sstream << "instances/" << problem_name << "/" << vertices_number << "/" 
+				    << cur_encoding << "/" << problem_name << vertices_number 
+			        << "_" << i << add_instance_name << ".cnf" << std::endl;
+		ofile.open(ofile_name.c_str());
+		ofile << sstream.rdbuf();
+		sstream.str(""); sstream.clear();
+		ofile.close(); ofile.clear();
+	}
+}
+
