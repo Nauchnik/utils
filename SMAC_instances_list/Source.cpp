df41773938df6ac1374b2fb4733945340755052d
Oleg Zaikin <zaikin.icc@gmail.com>
2017-03-18 01:00:05 +0800

    + function for Bivium

--- SMAC_instances_list/Source.cpp
+++ SMAC_instances_list/Source.cpp
@@ -5,19 +5,43 @@
 #include <vector>
 
 void makeInstancesLists(char *problem_name_char_arr, unsigned vertices_number, unsigned instances_number);
+void makeSimpleInstancesLists(char *folder_name_char_arr, char *problem_name_begin_char_arr, char *problem_name_end_char_arr, unsigned instances_number);
 
 int main()
 {
-	makeInstancesLists("BAR", 500,  100);
+	makeSimpleInstancesLists("Bivium_MANY_AUX_VARS_34known", "Bivium_MANY_AUX_VARS_200bits_template_0_", ".cnf", 100);
+
+	/*makeInstancesLists("BAR", 500,  100);
 	makeInstancesLists("BAR", 1000, 100);
 	makeInstancesLists("BAR", 2000, 10);
 	makeInstancesLists("BAR", 4000, 10);
 	makeInstancesLists("GNP", 500,  100);
 	makeInstancesLists("GNP", 1000, 100);
 	makeInstancesLists("GNP", 2000, 10);
-	makeInstancesLists("GNP", 4000, 10);
+	makeInstancesLists("GNP", 4000, 10);*/
 	
 	return 0;
+}
+
+void makeSimpleInstancesLists(char *folder_name_char_arr, char *problem_name_begin_char_arr, char *problem_name_end_char_arr, unsigned instances_number)
+{
+	std::string folder_name = folder_name_char_arr;
+	std::string problem_name_begin = problem_name_begin_char_arr;
+	std::string problem_name_end = problem_name_end_char_arr;
+	std::stringstream sstream;
+	std::string ofile_name;
+
+	sstream << problem_name_begin << "all";
+	ofile_name = sstream.str();
+	sstream.str(""); sstream.clear();
+	std::ofstream ofile(ofile_name.c_str());
+	
+	for (unsigned i = 0; i < instances_number; i++) {
+		sstream << "instances/" << folder_name << "/" << problem_name_begin << i << problem_name_end << std::endl;
+		ofile << sstream.rdbuf();
+		sstream.str(""); sstream.clear();
+	}
+	ofile.close(); ofile.clear();
 }
 
 void makeInstancesLists(char *problem_name_char_arr, unsigned vertices_number, unsigned instances_number)
