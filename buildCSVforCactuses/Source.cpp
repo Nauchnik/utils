6e0db8ee7491c8c72e055a2df1597314630dfdc7
Oleg Zaikin <zaikin.icc@gmail.com>
2017-08-22 02:10:06 +0800

    + buildCSVforCactuses

--- buildCSVforCactuses/Source.cpp
+++ buildCSVforCactuses/Source.cpp
@@ -7,11 +7,13 @@
 
 using namespace std;
 
+bool isNumber(string str);
+
 int main(int argc, char** argv)
 {
 #ifdef _DEBUG
 	argc = 3;
-	argv[1] = "testing_sat_solvers_out";
+	argv[1] = "gifford_minisat+minisat";
 	argv[2] = "out.csv";
 #endif
 	string file_name = argv[1];
@@ -73,6 +75,29 @@ int main(int argc, char** argv)
 		ofile << std::endl;
 	}
 	ofile.close();
-
+	
+	vector<string> shorten_cnfs_names;
+	for( auto& x : cnfs_names ) {
+		pos2 = x.find('.');
+		short_x = x.substr(0, pos2);
+		pos1 = short_x.find_last_of('_');
+		string str_index_2 = x.substr(pos1 + 1, (pos2 - pos1 - 1));
+		cout << str_index_2;
+		int pos2 = x.find_last_of('_');
+		string short_x = x.substr(0, pos2);
+		int pos1 = short_x.find_last_of('_');
+		string str_index_1 = x.substr(pos1 + 1, (pos2 - pos1 - 1));
+		cout << str_index_1;
+		shorten_cnfs_names.push_back(x.substr(0,pos1));
+	}
+	
 	return 0;
+}
+
+bool isNumber(string str)
+{
+	for (auto &x : str)
+		if (!isdigit(x))
+			return false;
+	return true;
 }
