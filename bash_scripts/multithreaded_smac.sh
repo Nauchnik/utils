02798bc1bb623d988e4dbcc815f8adafa6026a12
Oleg Zaikin <zaikin.icc@gmail.com>
2018-01-29 15:58:04 +0800

    code cleanup

--- bash_scripts/multithreaded_smac.sh
+++ bash_scripts/multithreaded_smac.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
+if [ "$#" -ne 3 ]; then
+    echo "Illegal number of parameters"
+    echo "Usage: scriptname filename threads_number node_index"
+    exit 1
+fi
 echo "Multithread SMAC starting."
-echo "Usage: scriptname filename threads"
 filename="$1"
 threads="$2"
+node_index="$3"
 output=""
 filename_base=""
-export SMAC_MEMORY=2048
-for i in $(seq 1 $threads)
+cur_thread_num=0
+export SMAC_MEMORY=7680
+for cur_thread_index in $(seq 1 $threads)
 do
+    cur_thread_num=$((node_index*threads+cur_thread_index)); 
+    echo $cur_thread_num;
     filename_base=$(basename $filename);
     #echo "filename_base" $filename_base
-    output=out_$filename_base"_"$i;
-    #echo "output" $output;
-    nohup ./smac --scenario-file $filename --shared-model-mode true --seed $i &> $output &
-    #echo "started smac # " $i;
+    output=out_$filename_base"_"$cur_thread_num;
+    echo "output" $output;
+    nohup ./smac --scenario-file $filename --shared-model-mode true --seed $cur_thread_num &> $output &
+    echo "started smac # " $cur_thread_num;
 done
+echo "SMAC_MEMORY" $SMAC_MEMORY
