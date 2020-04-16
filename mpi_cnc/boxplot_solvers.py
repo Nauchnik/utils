f165caccd3ab8e3c698739647bd4a0a35ea1f323
Oleg Zaikin <zaikin.icc@gmail.com>
2020-04-16 23:56:32 +0800

    ymax 5000 -> 5100

--- mpi_cnc/boxplot_solvers.py
+++ mpi_cnc/boxplot_solvers.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import sys
 
-timelimit = 5000
+timelimit = 5100
 solvers_names = ['MapleLCMDistChrBt-DL-v3', 'cadical_sr2019']
 
 f_name = sys.argv[1]
@@ -18,8 +18,7 @@ myFig = plt.figure();
 plt.ylim(0, timelimit)
 #bp = df.boxplot()
 _, bp = pd.DataFrame.boxplot(df, return_type='both')
-#bp = plt.boxplot(df)
-myFig.savefig("boxplot.pdf", format="pdf")
+myFig.savefig("boxplot_" + f_name.split('.')[0] + ".pdf", format="pdf")
 
 print(df.describe())
 whiskers = [whiskers.get_ydata() for whiskers in bp['whiskers']]
