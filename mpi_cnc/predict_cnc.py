0013d36ede3ab61ac0c3da7c0d71cf3163a14662
Oleg Zaikin <zaikin.icc@gmail.com>
2020-06-21 02:35:32 +0800

    *

--- mpi_cnc/predict_cnc.py
+++ mpi_cnc/predict_cnc.py
@@ -4,43 +4,35 @@ import os
 import time
 import multiprocessing as mp
 import pandas as pd
+import numpy as np
 import logging
 import find_cnc_n_param as fcnp
 
-input_vars_number = 512
-output_vars_number = 128 # last variable number in a CNF
+INPUT_VARS_NUM = 512 # first variables in a CNF
+OUTPUT_VARS_NUM = 128 # last variables in a CNF
+IN_OUT_PAIRS_NUM = 10000
+SIMP_SOLVER = './cadical130'
+ENUM_SOL_SOLVER = './cryptominisat5'
 CUBES_BOARD_FRAC = 0.25
 MARCH_BOARD_FRAC = 0.25
-solvers = ['./cryptominisat5.7.1', './cadical130', './v3', './kissat', './cube-glucose-min10sec-cad130.sh', './cube-glucose-min1min-cad130.sh', './cube-cad130-min10sec-cad130.sh', './cube-cad130-min1min-cad130.sh']
+service_processes = ['./timelimit', './iglucose', './cadical130', './march_cu']
+solvers = ['./kissat']
+#solvers = ['./cryptominisat5.7.1', './cadical130', './v3', './kissat', './cube-glucose-min10sec-cad130.sh', './cube-glucose-min1min-cad130.sh', './cube-cad130-min10sec-cad130.sh', './cube-cad130-min1min-cad130.sh']
 sh_solvers = [s for s in solvers if '.sh' in s]
-LING_MIN_TIME_LIMIT = 120
+SIMP_MIN_TIME_LIMIT = 60
 RANDOM_SAMPLE_SIZE = 30
 
 results = dict()
 interrupted = 0
 non_match_cubes = 0
+cnf_vars_number = -1
+template_cnf_clauses = []
 
 def clean_garbage():
-	logging.info('killing processes')
-	logging.debug('killing timelimit')
-	sys_str = 'killall -9 ./timelimit'
-	o = os.popen(sys_str).read()
-	logging.debug('killing solvers')
-	for solver in solvers:
-		sys_str = 'killall -9 ' + solver
+	logging.info('killing service processes')
+	for process in service_processes:
+		sys_str = 'killall -9 ' + process
 		o = os.popen(sys_str).read()
-	logging.debug('killing lingeling')
-	sys_str = 'killall -9 ./lingeling'
-	o = os.popen(sys_str).read()
-	logging.debug('killing march_cu')
-	sys_str = 'killall -9 ./march_cu'
-	o = os.popen(sys_str).read()
-	logging.debug('killing ilingeling')
-	sys_str = 'killall -9 ./ilingeling'
-	o = os.popen(sys_str).read()
-	logging.debug('killing iglucose')
-	sys_str = 'killall -9 ./iglucose'
-	o = os.popen(sys_str).read()
 	# kill solvers one more time to be sure
 	logging.debug('killing solvers')
 	for solver in solvers:
@@ -58,37 +50,29 @@ def clean_garbage():
 	sys_str = 'rm ./id-*'
 	o = os.popen(sys_str).read()
 
-def generate_random_values(template_cnf_name : str, cnf_id : int):
-	values = dict()
-	sys_str = './lingeling --seed=' + str(cnf_id) + ' ' + template_cnf_name
+def generate_random_values(template_cnf_name : str):
+	lst_values_all_vars = []
+	sys_str = ENUM_SOL_SOLVER + ' ' + template_cnf_name + ' --maxsol=' + str(IN_OUT_PAIRS_NUM)
 	o = os.popen(sys_str).read()
-	s = ''
-	lst = o.split('\n')
-	for x in lst:
-		if x == '':
-			continue
-		if x[0] == 'v':
-			s = x.split('v ')[1]
-			int_vals = [int(word) for word in s.split(' ')]
-			for iv in int_vals:
-				if iv != 0:
-					values[abs(iv)] = 1 if iv>0 else 0
-	return values
+	sat_blocks = o.split('s SATISFIABLE\n')
+	for s_b in sat_blocks:
+		lines = s_b.split('\n')
+		values = []
+		s = ''
+		for x in lines:
+			if x == '' or x[0] == 'c':
+				continue
+			if x[0] == 'v':
+				s = x.split('v ')[1]
+				int_vals = [int(word) for word in s.split(' ') if word != '']
+				for iv in int_vals:
+					if iv != 0:
+						values.append(1 if iv>0 else 0)
+		if len(values) > 0:
+			lst_values_all_vars.append(values)
+	return lst_values_all_vars
 
 def make_cnf_known_values(template_cnf_name : str, cnf_name : str, known_vars_values : dict):
-	cnf_vars_number = -1
-	template_cnf_clauses = []
-	with open(template_cnf_name, 'r') as ifile:
-		lines = ifile.readlines()
-		for line in lines:
-			if len(line) < 2 or line[0] == 'c':
-				continue
-			if 'p cnf' in line:
-				cnf_vars_number = int(line.split(' ')[2])
-				#print('cnf_vars_number : %d' % cnf_vars_number)
-			else:
-			    template_cnf_clauses.append(line)
-	#
 	cnf_clauses_number = len(template_cnf_clauses) + len(known_vars_values)
 	#print('cnf_clauses_number : %d' % cnf_clauses_number)
 	with open(cnf_name, 'w') as cnf:
@@ -140,7 +124,7 @@ def get_sat_cube(cubes_name, values_all_vars):
 			for word in lst:
 				val = 0 if word[0] == '-' else 1
 				var = abs(int(word))
-				if values_all_vars[var] == val:
+				if values_all_vars[var-1] == val:
 					match_number += 1
 			if match_number == len(lst):
 				sat_cubes.append(lst)
@@ -171,12 +155,12 @@ def add_cube(old_cnf_name : str, new_cnf_name : str, cube : list):
 	
 def make_cnf_known_sat_cube(n : int, cnf_name : str, values_all_vars : list, original_data_val : tuple):
 	min_cnf_name = 'min_' + cnf_name
-	sys_str = './lingeling -s -T ' + str(LING_MIN_TIME_LIMIT) + ' -o ' + min_cnf_name + ' ' + cnf_name
+	sys_str = SIMP_SOLVER + ' ' + cnf_name + ' -t ' + str(SIMP_MIN_TIME_LIMIT) + ' -o ' + min_cnf_name
 	#print('start system command : ' + sys_str)
 	o = os.popen(sys_str).read()
 	isSat = find_sat_log(o)
 	if isSat:
-		print('*** SAT found by lingeling on the minimization phase')
+		print('*** SAT was found on the minimization phase')
 		print(o)
 		exit(1)
 	remove_file(cnf_name)
@@ -288,16 +272,29 @@ def get_solver_march_time(o, min_time):
 			#logging.info('march_time : %f' % march_time)
 			break
 	return march_time
+
+def read_template_cnf(template_cnf_name : str):
+	global cnf_vars_number
+	global template_cnf_clauses
+	with open(template_cnf_name, 'r') as ifile:
+		lines = ifile.readlines()
+		for line in lines:
+			if len(line) < 2 or line[0] == 'c':
+				continue
+			if 'p cnf' in line:
+				cnf_vars_number = int(line.split(' ')[2])
+			else:
+			    template_cnf_clauses.append(line)
 	
-def solve_cnf_id(n : int, solvers : list, template_cnf_name : str, cnf_id : int, original_data_val : tuple):
-	#print('cnf_id : %d' % cnf_id)
-	values_all_vars = generate_random_values(template_cnf_name, cnf_id)
-	#print('%d values_all_vars : ' % len(values_all_vars))
-	#print(values_all_vars)
+def solve_cnf_id(n : int, solvers : list, template_cnf_name : str, cnf_id : int, original_data_val : tuple, values_all_vars):
+	if len(template_cnf_clauses) == 0:
+		read_template_cnf(template_cnf_name)
+
+	#print('values_all_vars len : %d' % len(values_all_vars))
 	output_vars = dict()
-	for i in range(len(values_all_vars)-output_vars_number,len(values_all_vars)):
+	for i in range(len(values_all_vars)-OUTPUT_VARS_NUM,len(values_all_vars)):
 		v = i+1
-		output_vars[v] = values_all_vars[v]
+		output_vars[v] = values_all_vars[i]
 	#print('%d output vars :' % len(output_vars))
 	cnf_name = 'rand_' + template_cnf_name.replace('./','').split('.')[0] + '_cnfid_' + str(cnf_id) + '.cnf'
 	make_cnf_known_values(template_cnf_name, cnf_name, output_vars)
@@ -320,7 +317,7 @@ def solve_cnf_id(n : int, solvers : list, template_cnf_name : str, cnf_id : int,
 				sys_str = solver + ' ' + cnf_known_sat_cube_name + ' ' + str(cnf_id) + ' ' + str(fcnp.SOLVER_TIME_LIMIT)
 			else:
 				sys_str = './timelimit -T 1 -t ' + str(fcnp.SOLVER_TIME_LIMIT) + ' ' + solver + ' ' + cnf_known_sat_cube_name
-			#print('start system command : ' + sys_str)
+			print('start system command : ' + sys_str)
 			t = time.time()
 			o = os.popen(sys_str).read()
 			t = time.time() - t
@@ -422,9 +419,15 @@ if __name__ == '__main__':
 	logging.info(sh_solvers)
 	logging.info('random sample size : %d' % RANDOM_SAMPLE_SIZE)
 
+	lst_values_all_vars = generate_random_values(template_cnf_name)
+	print('lst_values_all_vars len : %d' % len(lst_values_all_vars))
+
+	#solve_cnf_id(2460, solvers, template_cnf_name, 0, original_data_dict[2460], lst_values_all_vars[0])
+	#exit(1)
+
 	clean_garbage()
 	pool = mp.Pool(cpu_number)
-	
+
 	for n in original_data_dict:
 		logging.info(' ')
 		logging.info('***')
@@ -465,7 +468,8 @@ if __name__ == '__main__':
 			else:
 				logging.error('wrong case in main loop')
 				exit(-1)
-			pool.apply_async(solve_cnf_id, args=(n, solvers, template_cnf_name, cnf_id, original_data_dict[n]), callback=collect_result)
+			print('cnf_id : %d' % cnf_id)
+			pool.apply_async(solve_cnf_id, args=(n, solvers, template_cnf_name, cnf_id, original_data_dict[n], lst_values_all_vars[cnf_id]), callback=collect_result)
 			while len(pool._cache) >= cpu_number and len(results[n]) < RANDOM_SAMPLE_SIZE: # wait until any cpu is free
 				time.sleep(2)
 			if len(results[n]) >= RANDOM_SAMPLE_SIZE:
