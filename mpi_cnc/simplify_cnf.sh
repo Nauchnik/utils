3ca45897ecfda2f7184b4457d2161ba650515c36
Nauchnik <zaikin.icc@gmail.com>
2022-03-29 07:04:49 +0800

    Logging and more runs.

--- mpi_cnc/simplify_cnf.sh
+++ mpi_cnc/simplify_cnf.sh
@@ -2,11 +2,43 @@
 # Simplify a given CNF via the CaDiCaL solver.
 # Use two limits of the number of clauses: 1 and 1000000.
 # The first one corresponds to Unit Popagation.
+#
+# version 0.0.2
 
 cnf=$1
+log=simpl-$(basename $cnf)
+log=${log%.*}
+#
 newcnf=${cnf%.*}
 newcnf=$newcnf-1cnfl.cnf
-cadical_1.4.1 -c 1 $cnf -o $newcnf -q
+cadical_1.5 -c 1 $cnf -o $newcnf > $log
+echo -e "\n" >> $log
+#
+newcnf=${cnf%.*}
+newcnf=$newcnf-1thouscnfl.cnf
+cadical_1.5 -c 1000 $cnf -o $newcnf >> $log
+echo -e "\n" >> $log
+#
+newcnf=${cnf%.*}
+newcnf=$newcnf-10thouscnfl.cnf
+cadical_1.5 -c 10000 $cnf -o $newcnf >> $log
+echo -e "\n" >> $log
+#
+newcnf=${cnf%.*}
+newcnf=$newcnf-100thouscnfl.cnf
+cadical_1.5 -c 100000 $cnf -o $newcnf >> $log
+echo -e "\n" >> $log
+#
 newcnf=${cnf%.*}
 newcnf=$newcnf-1milcnfl.cnf
-cadical_1.4.1 -c 1000000 $cnf -o $newcnf -q
+cadical_1.5 -c 1000000 $cnf -o $newcnf >> $log
+echo -e "\n" >> $log
+#
+newcnf=${cnf%.*}
+newcnf=$newcnf-10milcnfl.cnf
+cadical_1.5 -c 10000000 $cnf -o $newcnf >> $log
+echo -e "\n" >> $log
+#
+newcnf=${cnf%.*}
+newcnf=$newcnf-100milcnfl.cnf
+cadical_1.5 -c 100000000 $cnf -o $newcnf >> $log
