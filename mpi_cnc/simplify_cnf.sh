7d8dabd49dac139c2c9aa757c6e5c09ce7e9f496
Nauchnik <zaikin.icc@gmail.com>
2022-05-18 07:01:33 +0800

    Exclude 1000 confl.
    For MD4 it is similar to 1 confl.

--- mpi_cnc/simplify_cnf.sh
+++ mpi_cnc/simplify_cnf.sh
@@ -3,7 +3,7 @@
 # Use two limits of the number of clauses: 1 and 1000000.
 # The first one corresponds to Unit Popagation.
 #
-# version 0.0.3
+# version 0.0.4
 
 cnf=$1
 log=simpl-$(basename $cnf)
@@ -14,10 +14,10 @@ newcnf=$newcnf-1cnfl.cnf
 cadical_1.5 -c 1 $cnf -o $newcnf > $log
 echo -e "\n" >> $log
 #
-newcnf=${cnf%.*}
-newcnf=$newcnf-1thouscnfl.cnf
-cadical_1.5 -c 1000 $cnf -o $newcnf >> $log
-echo -e "\n" >> $log
+#newcnf=${cnf%.*}
+#newcnf=$newcnf-1thouscnfl.cnf
+#cadical_1.5 -c 1000 $cnf -o $newcnf >> $log
+#echo -e "\n" >> $log
 #
 newcnf=${cnf%.*}
 newcnf=$newcnf-10thouscnfl.cnf
